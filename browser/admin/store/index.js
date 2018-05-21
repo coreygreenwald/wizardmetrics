142d2617112afec76b03ffced910628b4f976e81
Corey Greenwald <coreygreen1108@gmail.com>
2018-05-21 19:23:17 -0400

    fix bug on master

--- browser/admin/store/index.js
+++ browser/admin/store/index.js
@@ -3,9 +3,8 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'; 
-import data from './data';
 
-const reducer = combineReducers({user, data})
+const reducer = combineReducers({user})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
