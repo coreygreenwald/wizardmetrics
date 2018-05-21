d6a232c7bab9ad09c1c52be127ebd5477e58d3ee
Corey Greenwald <coreygreen1108@gmail.com>
2018-05-20 22:44:24 -0400

    fix port

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const server = require('./server');
 const { db } = require('./db');
 db.sync()
     .then(() => {
-        server.listen(process.env.PORT || 3000, () => console.log('server running on port 5000s'));
+        server.listen(process.env.PORT || 5000, () => console.log('server running on port 5000s'));
     })
     .catch(console.error)
