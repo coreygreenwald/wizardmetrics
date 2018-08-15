234d2755b532597eb62774ba3972ec3a01d4e70d
Corey Greenwald <coreygreen1108@gmail.com>
2018-08-15 15:23:35 -0400

    add data compression on backend

--- jobs/calculateJourneyInfo.js
+++ jobs/calculateJourneyInfo.js
@@ -1,34 +1,48 @@
 const { Action, Customer, db, Journey } = require('../db');
 const { journeys, compressor } = require('../utils');
-
+const client = require('../cache');
 const chalk = require('chalk');
 
 const calculateJourneyInfoRunner = async () => {
-    try {
-        const customers = await Customer.findAll();
-        for(let i = 0; i < customers.length; i++){
-            let customer = customers[i];
-            await customer.matchActionsToConversions()
-            let journey = await journeys(customer.username);
-            //call cacher on journey info
-            journey.customerPublicId = customer.publicId
-            let date = new Date();
-            let newDate = date.toISOString().replace(/[-:,TZ]/g, '')
-            journey.day = newDate.slice(0, newDate.length - 4); 
-            if(customer.name !== "DemoAccount"){
-                let journeyObj = await Journey.create(journey);
-                compressor(customer.username, journeyObj.id, journeyObj.info, {model: 'IMPACT', weight: 'MOST'}, {conversionType: 'BOTH'})
-            }
-            console.log(chalk.green('Journey Data Created for ', customer.username));
-        }
-        await db.close();
-    } catch(err){
-        console.log('There was an error running the job!', err);
-    }
+    // try {
+    //     const customers = await Customer.findAll();
+    //     for(let i = 0; i < customers.length; i++){
+    //         let customer = customers[i];
+    //         await customer.matchActionsToConversions()
+    //         let journey = await journeys(customer.username);
+    //         //call cacher on journey info
+    //         journey.customerPublicId = customer.publicId
+    //         let date = new Date();
+    //         let newDate = date.toISOString().replace(/[-:,TZ]/g, '')
+    //         journey.day = newDate.slice(0, newDate.length - 4); 
+    //         let journeyObj; 
+    //         if(customer.name !== "DemoAccount"){
+    //             journeyObj = await Journey.create(journey);
+    //             await compressor(customer.username, journeyObj.id, journeyObj.info, {model: 'IMPACT', weight: 'MOST'}, {conversionType: 'BOTH'})
+    //         }
+    //         await client.actions.setObj(`${customer.username}:JOURNEY:RECENT`, journeyObj || {})
+    //         console.log(chalk.green('Journey Data Created for ', customer.username));
+    //     }
+    //     // await db.close();
+    // } catch(err){
+    //     console.log('There was an error running the job!', err);
+    // }
 }
-db.sync()
-    .then(calculateJourneyInfoRunner)
-    .catch(() => {
-        console.log(chalk.red(new Date(), 'calculateJourneyInfo Job Failed'))
-    })
 
+(async function(){
+    try {
+        await calculateJourneyInfoRunner();
+        console.log(chalk.green('Mission Success!'))
+    } catch(err){
+        console.log(chalk.red(new Date(), 'calculateJourneyInfo Job Failed'))
+    } finally {
+        db.close(); 
+    }
+})()
+// db.sync()
+//     .then(calculateJourneyInfoRunner)
+//     .then(() => console.log(chalk.green('Mission Success!')))
+//     .catch(() => {
+//         console.log(chalk.red(new Date(), 'calculateJourneyInfo Job Failed'))
+//     })
+//     .finally(() => db.close())
--- server/admin-routes/data/journeys.js
+++ server/admin-routes/data/journeys.js
@@ -1,21 +1,49 @@
 const router = require('express').Router()
 const { Journey } = require('../../../db');
+const client = require('../../../cache');
 
-router.get('/', (req, res, next) => {
+
+router.get('/', async (req, res, next) => {
+    // && req.query.model
     if(req.customer){
-        Journey.findAll({
-            limit: 1,
-            where: {
-              customerPublicId: req.customer.publicId
-            },
-            order: [ [ 'createdAt', 'DESC' ]]
-          }).then(([journey]) => {
-              if(journey){
-                res.json(journey);
-              } else {
-                res.send('No data yet!'); 
-              }
-          }).catch(next);
+        try {
+            let recentJourney = await client.actions.getObj(`${req.customer.username}:JOURNEY:RECENT`);
+            //Fallback to making the query just to ensure there's no cached data.
+            if(!recentJourney || !Object.keys(recentJourney).length){
+                [recentJourney] = await Journey.findAll({
+                    limit: 1,
+                    where: {
+                        customerPublicId: req.customer.publicId
+                    },
+                    order: [ [ 'createdAt', 'DESC' ]]
+                })
+            }
+            if(recentJourney){
+                res.send(recentJourney); 
+            } else {
+                res.status(404).send('No Data Yet!')
+            }
+        } catch(err){
+            next(err);
+        }
+        
+
+        // client.actions.getObj(`${req.customer.username}:12:IMPACT-MOST`)
+        
+        // Journey.findAll({
+        //     limit: 1,
+        //     where: {
+        //       customerPublicId: req.customer.publicId
+        //     },
+        //     order: [ [ 'createdAt', 'DESC' ]]
+        //   }).then(([journey]) => {
+        //       console.log(journey)
+        //       if(journey){
+        //         res.json(journey);
+        //       } else {
+        //         res.send('No data yet!'); 
+        //       }
+        //   }).catch(next);
     } else {
         res.status(403).send('Action Forbidden!');
     }
