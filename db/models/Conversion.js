9033c96675cae578677cae8c82f9f36760c52b66
Corey Greenwald <coreygreen1108@gmail.com>
2018-08-16 16:38:18 -0400

    update compare paths method on conversion model

--- db/models/Conversion.js
+++ db/models/Conversion.js
@@ -21,13 +21,25 @@ const Conversion = db.define('conversion', {
 Conversion.prototype.compareActionToConversion = function(action){
     delete this.matchData.strength;
     let matchDataKeys = Object.keys(this.matchData);
-    return (action.type === this.type && action.path === this.path && (matchDataKeys.length > 0 ? (matchDataKeys.reduce((prev, key) => {
+    return (action.type === this.type && pathMatcher(this.path, action.path) && (matchDataKeys.length > 0 ? (matchDataKeys.reduce((prev, key) => {
         if(prev && this.matchData[key] === action.info[key]){
             return true; 
         } 
         return false;
     }, true))
     : true));
+}
+
+function pathMatcher(conversionPath, actionPath){
+    let cPath = conversionPath.slice(1).split('/');
+    let aPath = actionPath.slice(1).split('/'); 
+    if(cPath.length !== aPath.length) return false;
+    for(let i = 0; i < cPath.length; i++){
+        if(!cPath[i].startsWith(':') && cPath[i] !== aPath[i]){
+            return false;   
+        }
+    }
+    return true; 
 }
 
 module.exports = Conversion;
