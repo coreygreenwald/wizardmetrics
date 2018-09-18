9a9eaaf6f626dc563bab5e7d2fbeff81e05e5319
Corey Greenwald <coreygreen1108@gmail.com>
2018-09-18 10:09:06 -0400

    add customer info destroy job

---
+++ jobs/destroyCustomerInfo.js
@@ -0,0 +1,54 @@
+const { Action, Customer, Session, db, Journey } = require('../db');
+const client = require('../cache');
+const chalk = require('chalk');
+const Op = require('sequelize').Op;
+
+async function destroyInfo(username){
+    let startTime = new Date();
+    const customers = await Customer.findAll();
+    for(let i = 0; i < customers.length; i++){
+        let customer = customers[i]; 
+        if(customer.username === username){
+            let actionTimer = new Date();
+            let sessions = await customer.getSessions();
+            sessions = sessions.map(session => session.id)
+            await Action.destroy({
+                where: {
+                    sessionId: {
+                        [Op.in]: sessions
+                    }
+                }
+            })
+            console.log(chalk.yellow('Time for Action Deletion: ', new Date() - actionTimer));
+            let sessionDestroyTimer = new Date();
+            await Session.destroy({
+                where: {
+                    customerPublicId: customer.publicId
+                }
+            })
+            console.log(chalk.yellow('Time for Session Deletion: ', new Date() - sessionDestroyTimer));
+            let journeyDestroyTimer = new Date();
+            await Journey.destroy({
+                where: {
+                    customerPublicId: customer.publicId
+                }
+            })
+            console.log(chalk.yellow('Time for Journey Deletion: ', new Date() - journeyDestroyTimer));
+        
+            console.log(sessions.length)
+        }
+    }
+    console.log(chalk.green('Total Time:', new Date() - startTime))
+} 
+
+(async function(){
+    try {
+        await db.sync();
+        await destroyInfo(process.env.USERNAME);
+        console.log(chalk.green('Mission Success!'))
+    } catch(err){
+        console.log(chalk.red(new Date(), 'calculateJourneyInfo Job Failed'))
+    } finally {
+        db.close(); 
+    }
+})()
